6777a89bdde140eca42c0f73f1804b99b11e53ea
d-enk <d-enk@ya.ru>
2023-12-22 14:36:07 +0300

    mv box.stat to box/stat.lua

--- Library/box.lua
+++ Library/box.lua
@@ -183,35 +183,4 @@ function box.error.new(err) end
 ---@return BoxErrorObject
 function box.error.last() end
 
----@class BoxStatDefault
----@field total number
----@field rps number
-
----@class BoxStatDefaultWithCurrent:BoxStatDefault
----@field current number
-
----@class BoxStatNet
----@field SENT BoxStatDefault sent bytes to iproto
----@field RECEIVED BoxStatDefault received bytes from iproto
----@field CONNECTIONS BoxStatDefaultWithCurrent iproto connections statistics
----@field REQUESTS BoxStatDefaultWithCurrent iproto requests statistics
-
----@class BoxStat
----@field reset fun() # resets current statistics
----@field net fun(): BoxStatNet
----@overload fun(): BoxStatInfo
-box.stat = {}
-
----@class BoxStatInfo
----@field INSERT BoxStatDefault
----@field DELETE BoxStatDefault
----@field SELECT BoxStatDefault
----@field REPLACE BoxStatDefault
----@field UPDATE BoxStatDefault
----@field UPSERT BoxStatDefault
----@field CALL BoxStatDefault
----@field EVAL BoxStatDefault
----@field AUTH BoxStatDefault
----@field ERROR BoxStatDefault
-
 return box
--- Library/box/stat.lua
+++ Library/box/stat.lua
@@ -1,2 +1,48 @@
 ---@meta
 --luacheck: ignore
+
+---@class boxStat
+---@field reset fun() # resets current statistics
+---@field net fun(): boxStatNet # shows network activity
+---@field sql fun(): boxStatSql
+---@field vinyl fun(): boxStatVinyl # shows vinyl-storage-engine activity
+---@field memtx boxStatMemtx
+---@overload fun(): boxStatInfo
+box.stat = {}
+
+---@class boxStatDefault
+---@field total number
+---@field rps number
+
+---@class boxStatDefaultWithCurrent:boxStatDefault
+---@field current number
+
+---@class boxStatNet
+---@field SENT boxStatDefault sent bytes to iproto
+---@field RECEIVED boxStatDefault received bytes from iproto
+---@field CONNECTIONS boxStatDefaultWithCurrent iproto connections statistics
+---@field REQUESTS boxStatDefaultWithCurrent iproto requests statistics
+
+---@class boxStatSql
+-- TODO
+
+---@class boxStatVinyl
+-- TODO
+
+---@class boxStatMemtxTx
+--  TODO
+
+---@class boxStatMemtx
+---@field tx boxStatMemtxTx
+
+---@class boxStatInfo
+---@field INSERT boxStatDefault
+---@field DELETE boxStatDefault
+---@field SELECT boxStatDefault
+---@field REPLACE boxStatDefault
+---@field UPDATE boxStatDefault
+---@field UPSERT boxStatDefault
+---@field CALL boxStatDefault
+---@field EVAL boxStatDefault
+---@field AUTH boxStatDefault
+---@field ERROR boxStatDefault
